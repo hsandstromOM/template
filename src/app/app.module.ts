e221a7efeb81271e68498cb6ac36585e4295d404
hsandstromOM <hosea@obviouslee.com>
2018-01-16 12:39:31 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes }   from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectPageComponent } from './project-page/project-page.component';
+
+// check this and make a module out of it
+const routes: Routes = [
+  { path: '', component: HomePageComponent  },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'project-page',  component: ProjectPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    FooterComponent,
+    ProjectPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { useHash: false }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
