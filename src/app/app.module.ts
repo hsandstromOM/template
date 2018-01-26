961551ee8e2c47d829a392648f8b065ba223b137
hsandstromOM <hosea@obviouslee.com>
2018-01-26 17:17:07 -0500

    landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule, Routes }   from '@angular/router';
-// import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LandingComponent } from './landing/landing.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProjectPageComponent } from './project-page/project-page.component';
 
 // check this and make a module out of it
 const routes: Routes = [
-  { path: '', component: HomePageComponent  },
+  { path: '', component: LandingComponent  },
   { path: '', redirectTo: '', pathMatch: 'full' },
-  { path: 'project-page',  component: ProjectPageComponent }
+  // { path: 'project-page',  component: ProjectPageComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
+    LandingComponent,
     NavBarComponent,
     HomePageComponent,
     FooterComponent,
